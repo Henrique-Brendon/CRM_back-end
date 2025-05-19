63f356c55cdf4a928cf75c33d44e7e390abd8fe9
henrique <henriquebrendonti26@gmail.com>
2025-05-19 14:24:20 -0300

    Cors config

--- crm-service/src/main/java/com/henrique/crm_service/config/WebConfig.java
+++ crm-service/src/main/java/com/henrique/crm_service/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("https://crm-front-end-p1la.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
